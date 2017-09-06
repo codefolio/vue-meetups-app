4e8f8c5f3db6b5e4a9de9acb5f80b48d5a21baae
Flavio Oliveira <flavioho@me.com>
2017-09-06 09:26:32 +0200

    Add validation, firebase error msg and loading btns

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,10 @@
 <template>
   <v-container>
+    <v-layout row v-if="error">
+      <v-flex xs12 sm6 offset-sm3>
+        <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
+      </v-flex>
+    </v-layout>  
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
         <v-card>
@@ -30,7 +35,12 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex xs12>
-                    <v-btn primary block type="submit">Sign Up</v-btn>
+                    <v-btn primary block type="submit" :disabled="loading" :loading="loading">
+                      Sign In
+                      <span slot="loader" class="custom-loader">
+                        <v-icon light>cached</v-icon>
+                      </span>
+                    </v-btn>
                   </v-flex>
                 </v-layout>
               </form>
@@ -52,6 +62,12 @@
     computed: {
       user () {
         return this.$store.getters.user
+      },
+      error () {
+        return this.$store.getters.error
+      },
+      loading () {
+        return this.$store.getters.loading
       }
     },
     watch: {
@@ -64,6 +80,9 @@
     methods: {
       onSignin () {
         this.$store.dispatch('signUserIn', {email: this.email, password: this.password})
+      },
+      onDismissed () {
+        this.$store.dispatch('clearError')
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -32,7 +32,9 @@ export const store = new Vuex.Store({
         description: 'Rio, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus voluptatum minima aut enim eveniet, modi, labore voluptatem quod deserunt. Ratione odit, asperiores similique temporibus omnis quo sit incidunt. Facilis, at.'
       }
     ],
-    user: null
+    user: null,
+    loading: false,
+    error: null
   },
   mutations: {
     createMeetup (state, payload) {
@@ -40,6 +42,15 @@ export const store = new Vuex.Store({
     },
     setUser (state, payload) {
       state.user = payload
+    },
+    setLoading (state, payload) {
+      state.loading = payload
+    },
+    setError (state, payload) {
+      state.error = payload
+    },
+    clearError (state) {
+      state.error = null
     }
   },
   actions: {
@@ -56,9 +67,12 @@ export const store = new Vuex.Store({
       commit('createMeetup', meetup)
     },
     signUserUp ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
         .then(
           user => {
+            commit('setLoading', false)
             const newUser = {
               id: user.uid,
               registeredMeetups: []
@@ -68,11 +82,15 @@ export const store = new Vuex.Store({
         )
         .catch(
           error => {
+            commit('setLoading', false)
+            commit('setError', error)
             console.log(error)
           }
         )
     },
     signUserIn ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
       firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
         .then(
           user => {
@@ -85,9 +103,14 @@ export const store = new Vuex.Store({
         )
         .catch(
           error => {
+            commit('setLoading', false)
+            commit('setError', error)
             console.log(error)
           }
         )
+    },
+    clearError ({commit}) {
+      commit('clearError')
     }
   },
   getters: {
@@ -108,6 +131,12 @@ export const store = new Vuex.Store({
     },
     user (state) {
       return state.user
+    },
+    loading (state) {
+      return state.loading
+    },
+    error (state) {
+      return state.error
     }
   }
 })
